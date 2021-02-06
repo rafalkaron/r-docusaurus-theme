6f2288673468bb0ea58dc928f6d3ab7f53a47800
rafalkaron <rafalkaron@gmail.com>
2021-02-06 22:09:07 +0100

    added logo and favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,12 +5,12 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
   organizationName: 'rafalkaron', // Usually your GitHub org/user name.
   projectName: 'r-docusaurus-theme', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'R',
       logo: {
         alt: 'r-docusaurus-theme logo',
         src: 'img/logo.svg',
